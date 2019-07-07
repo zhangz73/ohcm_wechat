bfb631a0544dde140fae8de67c06ebbdddc04b1e
张稷 <zhangji@zhangjideMBP.dhcp4.washington.edu>
2019-07-07 16:18:58 -0700

    js first version

--- upload_script.js
+++ upload_script.js
@@ -1,29 +1,29 @@
 var http = require('http');
-
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
-    res.write('<input type="file" name="filetoupload"><br>');
-    res.write('<input type="submit">');
-    res.write('</form>');
-    return res.end();
-}).listen(8080);
-
+var fs = require('fs');
 var formidable = require('formidable');
 
+var upload_html = fs.readFileSync("Uploading_page.html");
+
+var upload_path = "qr_code/";
+
 http.createServer(function (req, res) {
-    if (req.url == '/fileupload') {
+    if (req.url == '/uploadform') {
+        res.writeHead(200);
+        res.write(upload_html);
+        return res.end();
+    } else if (req.url == '/fileupload') {
         var form = new formidable.IncomingForm();
         form.parse(req, function (err, fields, files) {
-            res.write('File uploaded');
-            res.end();
+            // oldpath : temporary folder to which file is saved to
+            var oldpath = files.qr_code.path;
+            var newpath = upload_path + files.qr_code.name;
+            // copy the file to a new location
+            fs.rename(oldpath, newpath, function (err) {
+                if (err) throw err;
+                // you may respond with another html page
+                res.write('File uploaded and moved!');
+                res.end();
+            });
         });
-    } else {
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
-        res.write('<input type="file" name="filetoupload"><br>');
-        res.write('<input type="submit">');
-        res.write('</form>');
-        return res.end();
     }
-}).listen(8080);
+}).listen(8086);
